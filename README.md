ab3461afe74874d93dec07de2b20ff9218bc3ba3
Fred Adewole <Fred Adewole>
2017-05-09 08:15:57 -0700

    Add index.js file, README, and test file to branch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # home-sessions-one
 A repo containing tasks for home session day one's labs
+to run, change directory to tests, then run command jasmine  testTypes.js
+
---
+++ app/index.js
@@ -0,0 +1,30 @@
+'use strict'
+module.exports = {
+	dataTypes: function(x) {
+		if(x === null) {
+			return 'no value';
+		} else if (typeof x === 'undefined') {
+			return 'no value';
+		} else if (typeof x === 'string') {
+			return x.length;
+		} else if (typeof x === 'boolean') {
+			return x;
+		} else if (typeof x === 'number') {
+			if (x < 100) {
+				return 'less than 100';
+			} else if (x > 100) {
+				return 'more than 100';
+			} else {
+				return 'equal to 100';
+			}
+		} else if (typeof x === 'object') {
+			if (x.length > 2) {
+				return x[2];
+			} else {
+				return undefined;
+			}
+		} else if (typeof x == 'function') {
+			return x(true);
+		}
+	}
+}
